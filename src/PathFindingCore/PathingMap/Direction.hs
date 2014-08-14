8853ca6b8464063c7c9f441f9516d75981c8a3e1
TheBizzle <jason.bertsche@gmail.com>
2014-08-14 18:33:23 -0500

    Deriving some free typeclasses

--- src/PathFindingCore/PathingMap/Direction.hs
+++ src/PathFindingCore/PathingMap/Direction.hs
@@ -4,7 +4,7 @@ module PathFindingCore.PathingMap.Direction where
     = North
     | East
     | South
-    | West
+    | West deriving (Eq, Show)
 
   directions :: [Direction]
   directions = [North, East, South, West]
