450a6428e540d8bb4d47284b7df48f5b67d9afb3
TheBizzle <jason.bertsche@gmail.com>
2014-08-16 17:58:54 -0500

    Providing an `Ord` instance for `PCoord`

--- src/PathFindingCore/PathingMap/Coordinate.hs
+++ src/PathFindingCore/PathingMap/Coordinate.hs
@@ -4,10 +4,10 @@ module PathFindingCore.PathingMap.Coordinate where
     = Coord { x :: Int, y :: Int } deriving (Eq, Show)
 
   data PriorityCoordinate
-    = PCoord { priority :: Int, coord :: Coordinate }
+    = PCoord { priority :: Int, coord :: Coordinate } deriving (Eq, Show)
 
-  compare :: PriorityCoordinate -> PriorityCoordinate -> Bool
-  compare (PCoord p1 _) (PCoord p2 _) = p1 < p2
+  instance Ord PriorityCoordinate where
+    compare (PCoord p1 _) (PCoord p2 _) = compare p1 p2
 
   data Breadcrumb
     = Breadcrumb { to :: Coordinate, from :: Coordinate }
