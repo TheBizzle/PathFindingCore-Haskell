793d77bad49ee6adcf27f8782928e7e405e8dd64
TheBizzle <jason.bertsche@gmail.com>
2014-08-22 01:43:23 -0500

    Rethinking `PriorityCoordinate`

--- src/PathFindingCore/PathingMap/Coordinate.hs
+++ src/PathFindingCore/PathingMap/Coordinate.hs
@@ -5,12 +5,6 @@ module PathFindingCore.PathingMap.Coordinate where
   data Coordinate
     = Coord { x :: Int, y :: Int } deriving (Eq, Ix, Ord, Show)
 
-  data PriorityCoordinate
-    = PCoord { priority :: Double, coord :: Coordinate } deriving (Eq, Show)
-
-  instance Ord PriorityCoordinate where
-    compare (PCoord p1 _) (PCoord p2 _) = compare p1 p2
-
   data Breadcrumb
     = Crumb { to :: Coordinate, from :: Breadcrumb }
-    | Source { source :: Coordinate }
+    | Source { source :: Coordinate } deriving (Eq, Show)
