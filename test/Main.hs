8a033f4411036b21bac8d211d19cd08c4ddc728a
TheBizzle <jason.bertsche@gmail.com>
2014-09-20 12:23:28 -0500

    Increasing my qualifications

--- test/Main.hs
+++ test/Main.hs
@@ -1,8 +1,9 @@
 module Main where
 
-import BreadcrumbTests  as Breadcrumb
-import InterpreterTests as IT
-import PathingMapTests  as PMT
+import qualified BreadcrumbTests  as Breadcrumb
+import qualified InterpreterTests as IT
+import qualified PathingMapTests  as PMT
+
 import Test.Framework.Runners.Console (defaultMain)
 
 main = defaultMain $ [IT.tests, PMT.tests, Breadcrumb.tests]
