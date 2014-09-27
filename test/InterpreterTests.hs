fbf9a97886dd5cd4695aadfded8a484af360188b
TheBizzle <jason.bertsche@gmail.com>
2014-09-27 01:25:12 -0500

    Adding test to cover recently-fixed bug

--- test/InterpreterTests.hs
+++ test/InterpreterTests.hs
@@ -25,12 +25,16 @@ tests = testGroup "Test interpreter" [
  , testInterpreter "2x2 grid 2"           "G*|  "     (1, 1) (0, 1) (1, 1) [Empty, Goal,  Empty, Self]
  , testInterpreter "2x2 grid 3"           "G*|DD"     (1, 1) (0, 1) (1, 1) [Wall,  Goal,  Wall,  Self]
  , testInterpreter "2x2 grid 4"           "DD|*G"     (0, 0) (1, 0) (1, 1) [Self,  Wall,  Goal,  Wall]
- , testInterpreter "5x5 grid 1"           m5x5p1      (3, 1) (1, 3) (4, 4) [Wall,  Wall,  Wall,  Wall,  Wall,  Wall, Empty, Empty, Goal, Wall, Wall,  Empty, Empty, Empty, Wall, Wall,  Self,  Empty, Empty, Wall, Wall,  Wall,  Wall,  Wall,  Wall]
- , testInterpreter "5x5 grid 2"           m5x5p2      (0, 0) (2, 4) (4, 4) [Self,  Wall,  Water, Empty, Empty, Wall, Wall,  Water, Wall, Wall, Empty, Empty, Water, Wall,  Goal, Empty, Water, Water, Wall,  Wall, Water, Water, Empty, Empty, Empty]
+ , testInterpreter "5x5 grid 1"           m5x5p1      (3, 1) (1, 3) (4, 4) [Wall,  Wall,  Wall,  Wall,  Wall,  Wall,  Empty, Empty, Goal,  Wall,  Wall,  Empty, Empty, Empty, Wall, Wall,  Self,  Empty, Empty, Wall, Wall,  Wall,  Wall,  Wall,  Wall]
+ , testInterpreter "5x5 grid 2"           m5x5p2      (0, 0) (2, 4) (4, 4) [Self,  Wall,  Water, Empty, Empty, Wall,  Wall,  Water, Wall,  Wall,  Empty, Empty, Water, Wall,  Goal, Empty, Water, Water, Wall,  Wall, Water, Water, Empty, Empty, Empty]
+ , testInterpreter "Past troublemaker"    trblmkr     (4, 2) (1, 1) (4, 2) [Water, Mound, Empty, Empty, Goal,  Water, Empty, Water, Empty, Empty, Empty, Empty, Empty, Water, Self]
  ]
  where
-   m5x5p1 = "DDDDD|DG  D|D   D|D  *D|DDDDD"
-   m5x5p2 = " DGD | DDD |%%%% |DD %%|*D  %"
+   m5x5p1  = "DDDDD|DG  D|D   D|D  *D|DDDDD"
+   m5x5p2  = " DGD | DDD |%%%% |DD %%|*D  %"
+   trblmkr = " %  *|\
+             \OG% %|\
+             \%    |"
 
 testInterpreter :: String -> String -> (Int, Int) -> (Int, Int) -> (Int, Int) -> [Terrain] -> API.Test
 testInterpreter desc strGrid (x1, y1) (x2, y2) (ux, uy) arr = testCase desc assertion
